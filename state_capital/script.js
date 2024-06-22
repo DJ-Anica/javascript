4df609d83928a0dc28813a30c79c71fbdcc16ae4
David Anica <106039559+DJ-Anica@users.noreply.github.com>
2024-06-21 22:44:49 -0700

    Create script.js

---
+++ state_capital/script.js
@@ -0,0 +1,71 @@
+const states_capitals = {
+    "Alabama": "Montgomery",
+    "Alaska": "Juneau",
+    "Arizona": "Phoenix",
+    "Arkansas": "Little Rock",
+    "California": "Sacramento",
+    "Colorado": "Denver",
+    "Connecticut": "Hartford",
+    "Delaware": "Dover",
+    "Florida": "Tallahassee",
+    "Georgia": "Atlanta",
+    "Hawaii": "Honolulu",
+    "Idaho": "Boise",
+    "Illinois": "Springfield",
+    "Indiana": "Indianapolis",
+    "Iowa": "Des Moines",
+    "Kansas": "Topeka",
+    "Kentucky": "Frankfort",
+    "Louisiana": "Baton Rouge",
+    "Maine": "Augusta",
+    "Maryland": "Annapolis",
+    "Massachusetts": "Boston",
+    "Michigan": "Lansing",
+    "Minnesota": "Saint Paul",
+    "Mississippi": "Jackson",
+    "Missouri": "Jefferson City",
+    "Montana": "Helena",
+    "Nebraska": "Lincoln",
+    "Nevada": "Carson City",
+    "New Hampshire": "Concord",
+    "New Jersey": "Trenton",
+    "New Mexico": "Santa Fe",
+    "New York": "Albany",
+    "North Carolina": "Raleigh",
+    "North Dakota": "Bismarck",
+    "Ohio": "Columbus",
+    "Oklahoma": "Oklahoma City",
+    "Oregon": "Salem",
+    "Pennsylvania": "Harrisburg",
+    "Rhode Island": "Providence",
+    "South Carolina": "Columbia",
+    "South Dakota": "Pierre",
+    "Tennessee": "Nashville",
+    "Texas": "Austin",
+    "Utah": "Salt Lake City",
+    "Vermont": "Montpelier",
+    "Virginia": "Richmond",
+    "Washington": "Olympia",
+    "West Virginia": "Charleston",
+    "Wisconsin": "Madison",
+    "Wyoming": "Cheyenne",
+  };
+  
+  function get_info(input_str) {
+      for (const [state, capital] of Object.entries(states_capitals)) {
+          if (input_str.toLowerCase() === capital.toLowerCase()) {
+              return `<p>The capital city of ${state} is located in: ${capital.toUpperCase()}</p>`;
+          } else if (input_str.toLowerCase() === state.toLowerCase()) {
+              return `<p>The capital of ${state} is: ${capital.toUpperCase()}</p>`;
+          }
+      }
+      return "<p>Not found.</p>";
+  }
+  
+  function getInfo() {
+      const userInput = document.getElementById("user-input").value.trim();
+      const outputDiv = document.getElementById("output");
+      const result = get_info(userInput);
+      outputDiv.innerHTML = result;
+  }
+  
